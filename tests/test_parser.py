99fcbd4c91c2f9112d1579ff9bb9fbcd80facfeb
bplutton <147344755+bplutton@users.noreply.github.com>
2026-02-13 20:45:05 +0000

    Added three tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -27,3 +27,30 @@ def test_parse_product_basic_returns_only_id_and_name(valid_product):
 
     # Assert
     assert set(response.keys()) == {"id", "name"}
+
+def test_parse_availability_when_in_stock(valid_product):
+    # Arrange
+
+    # Act
+    availability = parse_availability(valid_product)
+
+    # Assert
+    assert availability is True
+
+def test_parse_availability_when_out_of_stock(product_out_of_stock):
+    # Arrange
+
+    # Act
+    availability = parse_availability(product_out_of_stock)
+
+    # Assert
+    assert availability is False
+
+def test_parse_availability_when_field_missing(minimal_product):
+    # Arrange
+
+    # Act
+    availability = parse_availability(minimal_product)
+
+    # Assert
+    assert availability is False
